b95bed0008b4665c789e27818c3aa17a544d324a
Ragil <ragil@redpedia.com>
2019-04-02 15:02:20 +0700

    Latihan constructor

---
+++ Constructor/Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Mahasiswa
+    {
+        //constructor default
+        public Mahasiswa()
+        {
+
+        }
+
+        //constructor dengan 2 parameter
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Mahasiswa mhs1 = new Mahasiswa();
+            CetakHasil(mhs1);
+
+            Console.WriteLine(); // baris kosong
+
+            Mahasiswa mhs2 = new Mahasiswa("3456", "Rame");
+            CetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("Nim : {0}", mhs.Nim);
+            Console.WriteLine("Nama : {0}", mhs.Nama);
+
+
+        }
+    }
+}
